dbc3500700f65d7d9f103f4c140f38f1e56dcf28
thecontinium <thecontinium@outlook.com>
2026-05-03 16:02:42 +1000

    add a protocol

--- src/pez/java_hello.clj
+++ src/pez/java_hello.clj
@@ -1,8 +1,17 @@
 (ns pez.java-hello
   (:import [pez HelloWorld]))
 
+(defprotocol GOODBYE
+  (goodbye [this message]))
+
+(extend-type HelloWorld
+  GOODBYE
+  (goodbye [this message]
+    (println "goodbye " (.bar this) message)))
+
+(goodbye (HelloWorld.) "farewell")
+
 (comment
   HelloWorld/FOO
   (HelloWorld/foo)
-  (.bar (HelloWorld.))
-  )
+  (.bar (HelloWorld.)))
